ffdd79609ea423d59f2f971093b12f5e372f3d7c
Kingbnjmn <business@kingbnjmn.com>
2025-11-21 16:15:23 -0500

    Added "include.h"

--- source/PluginProcessor.h
+++ source/PluginProcessor.h
@@ -1,9 +1,8 @@
 #pragma once
 
-#include <juce_audio_processors/juce_audio_processors.h>
-#include <juce_dsp/juce_dsp.h>
+#include <includes.h>
 
-class PluginProcessor : public juce::AudioProcessor
+class PluginProcessor final : public juce::AudioProcessor
 
 {
 public:
