6e9a2ce1bc41b6e094a0ed107babb7dd27d87c40
MRSDN <business@kingbnjmn.com>
2026-02-11 20:22:18 -0500

    Init

---
+++ source/dsp/Engine.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#ifndef TAPDANCER_ENGINE_H
+#define TAPDANCER_ENGINE_H
+
+#include <JuceHeader.h>
+
+namespace MarsDSP::DSP {
+    template <typename SampleType>
+    class Engine {
+    public:
+
+    private:
+    };
+}
+#endif
